8b31932ac312955c8361b350374f2ddfefa4398b
CaioNery <caionery40@gmail.com>
2025-07-24 14:15:25 -0300

    Update appsettings.json

--- obj/Debug/net8.0/FarmaciaSOFT.AssemblyInfo.cs
+++ obj/Debug/net8.0/FarmaciaSOFT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FarmaciaSOFT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26d7503d558f55dfe4e0c51e3dad8a9ab2e7cc31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+883b9db2b871db61061b37be184155c9557924ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("FarmaciaSOFT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FarmaciaSOFT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
